bcd4de06612d27d7d5403ccb323793fc84288a64
gremarsl <44293039+gremarsl@users.noreply.github.com>
2019-02-18 22:48:06 +0100

    Update pubquiz.py
    insert picture and new slide

--- pubquiz.py
+++ pubquiz.py
@@ -1,15 +1,32 @@
 from pptx import Presentation
 from pptx.util import Inches, Pt
 
+#insert picture
+img_path = 'benefiz.jpg'
+
+#create presentation object 
 prs = Presentation()
+#layout settings
 blank_slide_layout = prs.slide_layouts[6]
-slide = prs.slides.add_slide(blank_slide_layout)
 
-left = top = width = height = Inches(1)
-txBox = slide.shapes.add_textbox(left, top, width, height)
+#add slide
+slide1 = prs.slides.add_slide(blank_slide_layout)
+slide2 = prs.slides.add_slide(blank_slide_layout)
+
+#scaling
+left = Inches(1)
+top = Inches(1)
+top1 = Inches(1.5)
+width = Inches(8)
+height = Inches(5.5)
+#add picture
+pic = slide1.shapes.add_picture(img_path, left, top1,width,height)
+slide1.shapes.add_textbox(left, top, width, height).text_frame.text="Willkommen zum Pubquiz der SOG LG_Stuttgart"
+
+#add textbox
+txBox = slide2.shapes.add_textbox(left, top, width, height)
 tf = txBox.text_frame
-
-tf.text = "This is text inside a textbox"
+tf.text = "Die LG_Stuttgart ist einfach die Beste ;-)"
 
 p = tf.add_paragraph()
 p.text = "HalliHallo"
@@ -19,4 +36,6 @@ p = tf.add_paragraph()
 p.text = "HalloHalli"
 p.font.size = Pt(40)
 
+
+#save file
 prs.save('pubquiz_LG_Stuttgart.pptx')
