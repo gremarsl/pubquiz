f9dd7772806171b0f32c1a406d74bad5cefb3da4
gremarsl <44293039+gremarsl@users.noreply.github.com>
2019-02-20 22:46:02 +0100

    Update pubquiz.py
    added question slides

--- pubquiz.py
+++ pubquiz.py
@@ -2,16 +2,27 @@ from pptx import Presentation
 from pptx.util import Inches, Pt
 
 #insert picture
-img_path = 'benefiz.jpg'
+img_path = 'pubquiz.png'
 
 #create presentation object 
 prs = Presentation()
 #layout settings
 blank_slide_layout = prs.slide_layouts[6]
 
-#add slide
-slide1 = prs.slides.add_slide(blank_slide_layout)
-slide2 = prs.slides.add_slide(blank_slide_layout)
+questions=list()
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+questions.append("Wie heißt Yoda’s Spezies?")
+
+
+
 
 #scaling
 left = Inches(1)
@@ -20,21 +31,51 @@ top1 = Inches(1.5)
 width = Inches(8)
 height = Inches(5.5)
 #add picture
-pic = slide1.shapes.add_picture(img_path, left, top1,width,height)
-slide1.shapes.add_textbox(left, top, width, height).text_frame.text="Willkommen zum Pubquiz der SOG LG_Stuttgart"
 
-#add textbox
-txBox = slide2.shapes.add_textbox(left, top, width, height)
-tf = txBox.text_frame
-tf.text = "Die LG_Stuttgart ist einfach die Beste ;-)"
+#first slide 
+first = prs.slides.add_slide(blank_slide_layout)
+pic = first.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
+first.shapes.add_textbox(left, top, width, height).text_frame.text="Willkommen zum Pubquiz der SOG LG_Stuttgart"
+#second slide - about us
+second = prs.slides.add_slide(blank_slide_layout)
+pic = second.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
+second.shapes.add_textbox(left, top, width, height).text_frame.text="AboutUs"
+#third slide - rules
+third = prs.slides.add_slide(blank_slide_layout)
+pic = third.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
+third.shapes.add_textbox(left, top, width, height).text_frame.text="Rules"
+#second slide - about us
 
-p = tf.add_paragraph()
-p.text = "HalliHallo"
-p.font.bold = True
+questionCounter=1
+for question in questions:
+    
+    slide = prs.slides.add_slide(blank_slide_layout)
+    left = Inches(1)
+    top = Inches(1)
+    top1 = Inches(1.5)
+    width = Inches(8)
+    height = Inches(5.5)
+    
+    #add textbox
+    txBox = slide.shapes.add_textbox(left, top, width, height)
+    tf = txBox.text_frame
+    tf.text = ""
 
-p = tf.add_paragraph()
-p.text = "HalloHalli"
-p.font.size = Pt(40)
+    frage = tf.add_paragraph()
+    frage.text = "Frage" + str(questionCounter)
+    frage.font.size = Pt(32)
+    frage.font.bold = True
+    questionTextBox = tf.add_paragraph()
+    questionTextBox.text = question + str(questionCounter)
+
+    answer = tf.add_paragraph()
+    answer.text = "ANSWER"
+
+                           
+    answer.text = "answer"
+    answer.font.size = Pt(32)
+    answer.font.bold = True
+    questionCounter+=1
 
 
 #save file
