0a91d80063c13928759e10851b73d23db3202cd3
gremarsl <44293039+gremarsl@users.noreply.github.com>
2019-03-22 21:06:22 +0100

    integrate files to every slide
    - added logo.png and pubquiz.png to every slide, modified size of answer textbox

--- pubquiz.py
+++ pubquiz.py
@@ -1,8 +1,11 @@
 from pptx import Presentation
 from pptx.util import Inches, Pt
 
+
 #insert picture
 img_path = 'pubquiz.png'
+zickzack='zickzack.png'
+logo='logo.png'
 
 #create presentation object 
 prs = Presentation()
@@ -87,58 +90,62 @@ answers.append(answer7)
 answers.append(answer8)
 answers.append(answer9)
 answers.append(answer10)
-print(answers)
 
 
 #scaling
-left = Inches(1)
+left = Inches(1)  
 top = Inches(1)
-top1 = Inches(1.5)
 width = Inches(8)
 height = Inches(5.5)
 #add picture
 
 #first slide 
 first = prs.slides.add_slide(blank_slide_layout)
-pic = first.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
-first.shapes.add_textbox(left, top, width, height).text_frame.text="Willkommen zum Pubquiz der SOG LG_Stuttgart"
+
+# Syntaxerklärung: img_path, Abstand des Bilds vom linken Folienrand, ... zum oberen Folienrand, Breite des Bildes ,Höhe des Bildes
+first.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
+first.shapes.add_textbox(left, top, width, height).text_frame.text="Willkommen zum Pubquiz der SOG LG_XXX"
 #second slide - about us
 second = prs.slides.add_slide(blank_slide_layout)
-pic = second.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
-second.shapes.add_textbox(left, top, width, height).text_frame.text="Hier kommen Informationen über uns"
+second.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
+second.shapes.add_textbox(left, top, width, height).text_frame.text="Hier könnt ihr Informationen über SOG zeigen"
 #third slide - rules
 third = prs.slides.add_slide(blank_slide_layout)
 pic = third.shapes.add_picture(img_path, Inches(1), Inches(1.5),Inches(8),Inches(5.5))
-third.shapes.add_textbox(left, top, width, height).text_frame.text="Hier kommen die Regeln"
+third.shapes.add_textbox(left, top, width, height).text_frame.text="Hier könnt ihr Regeln definieren"
 #second slide - about us
 
 questionCounter=1
+left = Inches(2.5)
+top = Inches(1)
+width = Inches(8)
+height = Inches(5.5)
 for i in range(10):
     
     slide = prs.slides.add_slide(blank_slide_layout)
-    left = Inches(1)
-    top = Inches(1)
-    top1 = Inches(1.5)
-    width = Inches(8)
-    height = Inches(5.5)
+    slide.shapes.add_picture(zickzack,Inches(0), Inches(0),Inches(2),Inches(8))
+    slide.shapes.add_picture(logo,Inches(8), Inches(5.5),Inches(2),Inches(1.6))
+
+
     
     #add textbox
     txBox = slide.shapes.add_textbox(left, top, width, height)
     tf = txBox.text_frame
-    tf.text = ""
+    tf.text = "SOG Pubquiz"
 
     frage = tf.add_paragraph()
     frage.text = "Frage" + str(questionCounter)
-    frage.font.size = Pt(32)
+    frage.font.size = Pt(40)
     frage.font.bold = True
     questionTextBox = tf.add_paragraph()
+    questionTextBox.size=Pt(40)
+    frage.font.bold=True
     questionTextBox.text = questions[i]
 
     answer = tf.add_paragraph()
-    answer.text = ""
     
     #TODO: extract answers and write it into the textbox                       
-    answer.text = "answers"
+    answer.text = str(answers[i])
     answer.font.size = Pt(32)
     answer.font.bold = True
     questionCounter+=1
