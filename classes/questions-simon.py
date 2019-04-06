653a75bf922c6d38ed7f5928c93fc5d66fbf8ab8
sheimbach <simon.heimbach@t-online.de>
2019-04-06 17:54:25 +0200

    Getter Setter

--- classes/questions-simon.py
+++ classes/questions-simon.py
@@ -25,6 +25,8 @@ class qround:
 class question:
     question=""
     answers={}
+    _image=""
+    _comment=""
     
     def __init__(Self, q):
         Self.question=q
@@ -32,6 +34,18 @@ class question:
     def addAnswer(Self, answer, correct):
         Self.answers[answer]=correct;
         
+    def setImage(Self, image):
+        Self._image=image
+
+    def getImage(Self):
+        return Self._image
+        
+    def setComment(Self, comment):
+        Self._comment=comment
+    
+    def getComment(Self):
+        return Self._comment
+    
     def dump(Self):
         print(Self.question)
         for a in Self.answers:
