b697d46751c19485c32744efd81ba083ae58db0c
Vinícius Mazzola <vinimaz@gmail.com>
2019-04-06 18:03:40 +0200

    Adding new methods to the layout class. Adding the pptx used for the layout.

--- classes/layout.py
+++ classes/layout.py
@@ -4,16 +4,17 @@ from enum import Enum
 # layout file name
 fileName_layout = "layout.pptx"
 
-# for testing purposes
-fileName_output = "outputTest.pptx"
-
 # ENUM used to define where are the layouts in the pptx
 class _slideIndex(Enum):
     TITLE_SLIDE = 0
     SOG_AD = 1
     RULES = 2
-    QUESTION_OPEN = 3
-    QUESTION_ALTERNATIVE = 4
+    ROUND_TITLE = 3
+    QUESTION_OPEN = 4
+    QUESTION_ALTERNATIVE = 5
+    QUESTION_MIDDLE = 6
+    QUESTION_MIDDLE_ANSWER = 7
+    RESULTS = 7
 
 
 
@@ -40,3 +41,46 @@ class Layout:
         :return: Slide object
         """
         return self.prs.slides[_slideIndex.SOG_AD]
+
+    def getGameRules(self):
+        """
+        Get the slide with the game rules
+        :return: Slide Object
+        """
+        return self.prs.slides[_slideIndex.RULES]
+    def getQuestionOpen(self):
+        """
+        Get the slide with a layout for open (not multiple choice) question
+        :return: Slide Object
+        """
+        return self.prs.slides[_slideIndex.QUESTION_OPEN]
+    def getQuestionMultipleChoices(self):
+        """
+        Get the slide with a layout for multiple choice questions
+        :return: Slide Object
+        """
+        return self.prs.slides[_slideIndex.QUESTION_ALTERNATIVE]
+    def getRoundTitle(self):
+        """
+        The slide used at the beginning of every round
+        :return: Slide Object
+        """
+        return self.prs.slides[_slideIndex.ROUND_TITLE]
+    def getQuestionMiddle(self):
+        """
+        The slide used in-between the rounds (Zwischenfrage)
+        :return: Slide Object
+        """
+        return self.prs.slides[_slideIndex.QUESTION_MIDDLE]
+    def getQuestionMiddleAnswer(self):
+        """
+        The slide used to show the answer for the in-between questions
+        :return: Slide Object
+        """
+        return self.prs.slides[_slideIndex.QUESTION_MIDDLE_ANSWER]
+    def getResults(self):
+        """
+        The slide used to show the charts with results
+        :return: Slide Object
+        """
+        return self.prs.slides[_slideIndex.RESULTS]
